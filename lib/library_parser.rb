08d8bb54b3d69d506763dd4c430377e082973ecd
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-05-12 13:19:43 -0400

    1 more

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class LibraryParser
   def files
-    data_path = File.join(File.dirname(__FILE__), "..", "db", "data")
+    data_path = File.join(File.dirname(__FILE__), '..', 'db', 'data')
     Dir.entries(data_path)[2..-1]
   end
 
   def self.parse
-    new.call
+    self.new.call
   end
 
   def parse_filename(filename)
